826b2b99f40aa8baca1bd9f3789998a862a63366
tinonazareth <zingidu@gmail.com>
2023-11-21 15:37:11 -0300

    first commit

---
+++ aula_javascript/app.js
@@ -0,0 +1,28 @@
+let number1 = 18;
+let number2 = 35;
+ let somaDosResulatdos = number1 + number2
+
+ console.log("A soma de " + number1 + " mais "+ number2 + " é igual = " + somaDosResulatdos);
+
+ console.clear()
+
+var str1 = 'hello';
+var str2 = 'world';
+var res = `${str1} ${str2}`;
+console.log(res);
+
+console.clear()
+
+let frutas = ['maça', 'banana', 'uva', 'abacate']
+console.log(frutas [3]);
+
+console.clear()
+
+let pessoa = {
+    nome: "Valtino",
+    idade: 47,
+    cidade: "Mauá",
+}
+
+console.log("Dados da pessoa: ", pessoa);
+
---
+++ aula_javascript/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let number1 = 18;
+// let number2 = 35;
+//  let somaDosResulatdos = number1 + number2
+
+//  console.log("A soma de " + number1 + " mais "+ number2 + " é igual = " + somaDosResulatdos);
+
+//  console.clear()
